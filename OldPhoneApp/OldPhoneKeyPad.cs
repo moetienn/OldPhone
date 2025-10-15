b214ea2aba5cdc20de573b7da6b236f6f3672997
root <root@DESKTOP-138FJF6.localdomain>
2025-10-15 13:14:56 +0700

    working basic keypad

---
+++ OldPhoneApp/OldPhoneKeyPad.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Data.Common;
+using System.Dynamic;
+
+namespace OldPhoneKeyPad
+{
+    public class KeypadDictionary
+    {
+        public static readonly Dictionary<char, string> Map = new()
+	    {
+			{'1', ""},
+			{'2', "ABC"},
+			{'3', "DEF"},
+			{'4', "GHI"},
+			{'5', "JKL"},
+			{'6', "MNO"},
+			{'7', "PQRS"},
+			{'8', "TUV"},
+			{'9', "WXYZ"},
+			{'0', " "}
+		};
+	}
+	class Keypad
+	{
+		public static String OldPhonePad(string input)
+		{
+			string result = "";
+			char lastChar = '\0';
+			int count = 0;
+			for (int i = 0; i < input.Length; i++)
+       		{
+				char c = input[i];
+                if (c == '#')
+                    break;
+                if (c == ' ')
+                {
+                    result += GetLetterFromDictionnary(lastChar, count);
+                    lastChar = '\0';
+                    count = 0;
+                }
+                if (c == lastChar)
+                    count++;
+                else
+                {
+                    if (lastChar != '\0')
+                        result += GetLetterFromDictionnary(lastChar, count);
+                    lastChar = c;
+                    count = 1;
+                }
+        	}
+
+            if (lastChar != '\0')
+                result += GetLetterFromDictionnary(lastChar, count);
+			return result;
+		}
+
+        private static char GetLetterFromDictionnary(char key, int count)
+        {
+            if (!KeypadDictionary.Map.TryGetValue(key, out var letters) || letters.Length == 0)
+                return key;
+
+            int index = (count - 1) % letters.Length;
+                return letters[index];
+        }
+	}
+}
---
+++ OldPhoneApp/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace OldPhoneKeyPad
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(Keypad.OldPhonePad(args[0]));
+		}
+	}
+}
